eec1e69dd92ccc86961b8fc40f8334f873617dce
Tim Preston <tim.preston@uitlanders.com>
2015-04-28 22:02:01 +1000

    Update to example from suave homepage
    Checking how azure hosting handles more complicated suave

--- build.fsx
+++ build.fsx
@@ -1,30 +1,37 @@
-// --------------------------------------------------------------------------------------
-// FAKE build script
-// --------------------------------------------------------------------------------------
-
-#r @"packages/FAKE/tools/FakeLib.dll"
-
+// Step 0. Boilerplate to get the paket.exe tool
+ 
 open System
 open System.IO
-open Fake
  
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+ 
+if not (File.Exists "paket.exe") then
+let url = "https://github.com/fsprojects/Paket/releases/download/0.31.5/paket.exe"
+use wc = new Net.WebClient() in let tmp = Path.GetTempFileName() in wc.DownloadFile(url, tmp); File.Move(tmp,Path.GetFileName url)
+ 
+// Step 1. Resolve and install the packages
+ 
+#r "paket.exe"
+ 
+Paket.Dependencies.Install """
+source https://nuget.org/api/v2
+nuget Suave
+nuget FSharp.Data
+nuget FSharp.Charting
+""";;
  
 // Step 2. Use the packages
  
 #r "packages/Suave/lib/net40/Suave.dll"
+#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#r "packages/FSharp.Charting/lib/net40/FSharp.Charting.dll"
+ 
+let ctxt = FSharp.Data.WorldBankData.GetDataContext()
+ 
+let data = ctxt.Countries.Algeria.Indicators.``GDP (current US$)``
  
 open Suave // always open suave
 open Suave.Http.Successful // for OK-result
 open Suave.Web // for config
-open Suave.Types
-open System.Net
-
-let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083" 
-
-let serverConfig = 
-    { defaultConfig with
-       bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
-    }
  
-startWebServer serverConfig (OK "Hello World! It's Suave.io on Azure Websites. <a href='https://github.com/shanselman/suavebootstrapper'>So easy to setup. Just click Deploy.</a>")
+startWebServer defaultConfig (OK (sprintf "Hello World! (example from http://suave.io/ ) In 2010 Algeria earned %f " data.[2010]))
